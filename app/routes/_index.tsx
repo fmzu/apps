9ac6e36d2c0d77918d9acb8b1fdd3b113af81932
fmzu <fjtmzk0723@gmail.com>
2024-05-09 15:58:34 +0900

    update: ページを修正する

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -10,7 +10,7 @@ export const meta: MetaFunction = () => {
 
 export default function Index() {
   return (
-    <div className="flex p-8">
+    <div className="flex space-x-4 p-8">
       <TopCard />
     </div>
   )

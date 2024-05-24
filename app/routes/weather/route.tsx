df41f086823366858b1681ce6cba380c64f0ce76
fmzu <fjtmzk0723@gmail.com>
2024-05-24 17:37:40 +0900

    update: ページを追加する

--- app/routes/weather/route.tsx
+++ app/routes/weather/route.tsx
@@ -1,32 +1,44 @@
+import { useState, useEffect } from "react"
 import "./styles.css"
 
 export default function WeatherPage() {
-  fetch(
-    "https://api.open-meteo.com/v1/forecast?latitude=35.6785&longitude=139.6823&hourly=temperature_2m&timezone=Asia%2FTokyo",
-  )
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error("ネットワークエラーが発生しました")
-      }
-      return response.json()
-    })
-    .then((data) => {
-      console.log("取得したデータ:", data)
-      data.hourly.temperature_2m.forEach(
-        (temperature: number, index: string) => {
-          console.log(
-            `時間: ${data.hourly.time[index]} - 気温: ${temperature}°C`,
-          )
-        },
-      )
-    })
-    .catch((error) => {
-      console.error("Fetch APIでエラーが発生しました:", error)
-    })
+  const [data, setData] = useState(null)
+
+  useEffect(() => {
+    fetch(
+      "https://api.open-meteo.com/v1/forecast?latitude=35.6895&longitude=139.6917&current=temperature_2m&hourly=temperature_2m&timezone=Asia%2FTokyo&forecast_days=1",
+    )
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("ネットワークエラーが発生しました")
+        }
+        return response.json()
+      })
+      .then((data) => {
+        setData(data)
+      })
+      .catch((error) => {
+        console.error("Fetch APIでエラーが発生しました:", error)
+      })
+  }, [])
 
   return (
     <div>
-      <p>{"お天気予報"}</p>
+      {data ? (
+        <div>
+          <h1>{"現在の東京の天気"}</h1>
+          <p>
+            {`気温:
+            ${
+              (data as { hourly: { temperature_2m: number[] } }).hourly
+                .temperature_2m[0]
+            }
+            ℃`}
+          </p>
+        </div>
+      ) : (
+        <p>{"読み込み中..."}</p>
+      )}
     </div>
   )
 }
